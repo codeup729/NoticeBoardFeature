b5944836cf8c0423e024287268265782dc7189ff
Anitej Srivastava <anitej.srivastava@byjus.com>
2023-01-19 18:50:01 +0530

    Initial Commit

---
+++ NoticeBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoticeBoard
+//
+//  Created by tnluser on 19/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
