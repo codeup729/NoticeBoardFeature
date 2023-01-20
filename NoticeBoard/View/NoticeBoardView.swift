52844df30cc7edf9b927519b870f26794eb0454e
Anitej Srivastava <anitej.srivastava@byjus.com>
2023-01-20 19:14:09 +0530

    Created View Controller

--- NoticeBoard/View/NoticeBoardView.swift
+++ NoticeBoard/View/NoticeBoardView.swift
@@ -10,5 +10,19 @@ import UIKit
 class NoticeBoardView: UIView {
 
     
-
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    
+    @IBOutlet weak var slideNumberLabel: UILabel!
+    
+    @IBOutlet weak var noticeboardCollectionView: UICollectionView!
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
