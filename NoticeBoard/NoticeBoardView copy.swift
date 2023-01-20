0b3163966a0a3c0e8073afcf9af42339ce91a8d8
Anitej Srivastava <anitej.srivastava@byjus.com>
2023-01-20 19:06:33 +0530

    Create xib file

---
+++ NoticeBoard/NoticeBoardView copy.swift
@@ -0,0 +1,8 @@
+//
+//  NoticeBoardView.swift
+//  NoticeBoard
+//
+//  Created by tnluser on 19/01/23.
+//
+
+import Foundation
---
+++ NoticeBoard/View/NoticeBoardView.swift
@@ -0,0 +1,14 @@
+//
+//  NoticeBoardView.swift
+//  NoticeBoard
+//
+//  Created by tnluser on 20/01/23.
+//
+
+import UIKit
+
+class NoticeBoardView: UIView {
+
+    
+
+}
